f0feac30dd6d8d6d150adda024a7c3cddd2813ec
Thiago Lopes <aslthiago@gmail.com>
2022-06-30 23:14:12 -0300

    init.lua: download packer automatically

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 require 'config'
 require 'keymap'
+
+
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    'git', 'clone', '--depth', '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  }
+
+  vim.notify 'Installing the plugin manager. Please restart Neovim.'
+  vim.cmd [[packadd packer.nvim]]
+end
