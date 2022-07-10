015cf0a8e8360c60ea618ffc469809277a7f852d
Thiago Lopes <aslthiago@gmail.com>
2022-07-10 15:20:22 -0300

    treesitter.lua: integration with comments plugin

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -78,4 +78,8 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
 }
